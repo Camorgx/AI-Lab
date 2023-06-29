a5224ca8119b1d0f739952ca3548aa92c52c6186
星河 <1821365384@qq.com>
2023-06-29 16:06:36 +0800

    code format

--- Lab2/part_1/src/Bayesian-network.py
+++ Lab2/part_1/src/Bayesian-network.py
@@ -1,13 +1,13 @@
 import numpy as np
-import pandas as pd
+
 
 class BayesianNetwork:
     def __init__(self, n_labels=10, n_pixels=784, n_values=2) -> None:
-        '''
+        """
         n_labels: number of labels, 10 for digit recognition
-        n_pixels: number of pixels, 256 for 16x16 image
+        n_pixels: number of pixels, 784 for 28x28 image
         n_values: number of values for each pixel, 0 for black, 1 for white
-        '''
+        """
         self.n_labels = n_labels
         self.n_pixels = n_pixels
         self.n_values = n_values
@@ -16,37 +16,34 @@ class BayesianNetwork:
         self.pixels_prior = np.zeros((n_pixels, n_values))
         # conditional probability
         self.pixels_cond_label = np.zeros((n_pixels, n_values, n_labels))
-    
 
     # fit the model with training data
     def fit(self, pixels, labels):
-        '''
+        """
         pixels: (n_samples, n_pixels, )
         labels: (n_samples, )
-        '''
+        """
         n_samples = len(labels)
         # TODO: calculate prior probability and conditional probability
-        
 
     # predict the labels for new data
     def predict(self, pixels):
-        '''
+        """
         pixels: (n_samples, n_pixels, )
         return labels: (n_samples, )
-        '''
+        """
         n_samples = len(pixels)
         labels = np.zeros(n_samples)
         # TODO: predict for new data
 
         return labels
-    
 
     # calculate the score (accuracy) of the model
     def score(self, pixels, labels):
-        '''
+        """
         pixels: (n_samples, n_pixels, )
         labels: (n_samples, )
-        '''
+        """
         n_samples = len(labels)
         labels_pred = self.predict(pixels)
         return np.sum(labels_pred == labels) / n_samples
